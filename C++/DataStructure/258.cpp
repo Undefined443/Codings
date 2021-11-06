d1685729498cc9a381ce0c8bd382cf8cc172836d
李潇 <iPhone4s2008@iCloud.com>
2021-11-07 01:15:42 +0800

    完成了 258

--- C++/DataStructure/258.cpp
+++ C++/DataStructure/258.cpp
@@ -1,34 +1,54 @@
 #pragma GCC diagnostic error "-std=c++14"
 
 #include <iostream>
-#include <forward_list>
 #include <vector>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
 class Node {
 public:
-  int address;
-  int val;
-  int next;
-  
-  Node() {
-    cin >> address;
-    cin >> val;
-    cin >> next;
-  }
+  string m_address;
+  int m_val;
+  string m_next;
+
+  Node(string address, int val, string next) : m_address(address), m_next(next), m_val(val) {};
 };
 
 int main() {
-    int address;
+    string begin;
     int n;
-    cin >> address >> n;
+    cin >> begin >> n;
     vector<Node> NodeVec;
     for (int i = 0; i < n; ++i) {
-      NodeVec.emplace();
+        string address;
+        int val;
+        string next;
+        cin >> address >> val >> next;
+        Node temp(address, val, next);
+      NodeVec.push_back(temp);
     }
-    forward_list<int> intList;
-    while (address != -1) {
-      NodeVec.find(NodeVec.begin(), NodeVec.end(), )
+    //整理链表
+    vector<Node> sortedVec;
+    string next = begin;
+    while (next != "-1") {
+        auto i = find_if(NodeVec.begin(), NodeVec.end(), [next](const Node &node)->bool{return node.m_address == next;});
+        sortedVec.push_back(*i);
+        next = i->m_next;
+    }
+    //整理链表
+    vector<Node> uniqueVec;
+    uniqueVec.push_back(sortedVec.front());
+    for (Node node : sortedVec) {
+        if (find_if(uniqueVec.begin(), uniqueVec.end(), [node](Node uni)->bool{return abs(node.m_val) == abs(uni.m_val);}) == uniqueVec.end()) { //如果没出现过
+            uniqueVec.back().m_next = node.m_address;
+            uniqueVec.push_back(node);
+        }
+    }
+    uniqueVec.back().m_next = "-1";
+    cout << uniqueVec.size() << endl;
+    for (Node node : uniqueVec) {
+        cout << node.m_address << " " << node.m_val << " " << node.m_next << endl;
     }
 }
