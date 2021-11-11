990ae1e1624e12676a20744e5aaab288f85e005e
李潇 <iPhone4s2008@iCloud.com>
2021-11-11 19:49:53 +0800

    4 正确，1 错误

--- C++/DataStructure/293.cpp
+++ C++/DataStructure/293.cpp
@@ -1,4 +1,4 @@
-//#pragma GCC diagnostic error "-std=c++14"
+#pragma GCC diagnostic error "-std=c++14"
 
 #include <iostream>
 #include <queue>
@@ -6,60 +6,43 @@
 using namespace std;
 
 int main() {
-  int n;
-  cin >> n;
-  queue<int> que;
-  for (int i = 1; i <= n; ++i) {
-    que.push(i);
-  }
-  que.push(0); //用于标记队尾
-  que.push(0);
-  que.push(0);
-  int front;
-  while (que.size() > 6) {
-  while ((front = que.front()) && que.size() > 6) {
-    //数到 1 的人出队再入队
-    que.pop();
-    que.push(front);
-    //数到 2 的人出队
-    que.pop();
-  }
-  if (que.size() <= 6) break;
-  //把标记挪回队尾
-  while (!(front = que.front())) {
-    que.pop();
-  }
-  que.push(0);
-  que.push(0);
-  que.push(0);
-  
-  while ((front = que.front()) && que.size() > 6) {
-    //数到 1 和 2 的人出队再入队
-    que.pop();
-    que.push(front);
-    front = que.front();
-    que.pop();
-    que.push(front);
-    //数到 3 的人出队
-    que.pop();
-  }
-  while (!(front = que.front())) {
-    que.pop();
-  }
-  que.push(0);
-  que.push(0);
-  que.push(0);
-  }
-  //调整队头
-  while (front = que.front()) {
-    que.pop();
-    que.push(front);
-  }
-  while (!(front = que.front())) {
-    que.pop();
-  }
-  for (int i = 0; i < 3; ++i) {
-    cout << que.front() << " ";
-    que.pop();
-  }
-  }
+    int n;
+    cin >> n;
+    queue<int> que;
+    for (int i = 1; i <= n; ++i) {
+        que.push(i);
+    }
+    int front;
+    while (que.size() > 3) {
+        auto len = que.size();
+        for (int i = 1; i <= len && que.size() > 3; ++i) {
+            if (i % 2 == 0) {
+                que.pop();
+            } else {
+                front = que.front();
+                que.pop();
+                que.push(front);
+            }
+        }
+        if (que.size() <= 3) break;
+        len = que.size();
+        for (int i = 1; i <= len && que.size() > 3; ++i) {
+            if (i % 3 == 0) {
+                que.pop();
+            } else {
+                front = que.front();
+                que.pop();
+                que.push(front);
+            }
+        }
+    }
+    //调整队头
+    while ((front = que.front()) != 1) {
+        que.pop();
+        que.push(front);
+    }
+    for (int i = 0; i < (n < 3 ? n : 3); ++i) {
+        cout << que.front() << " ";
+        que.pop();
+    }
+}
