73611e80bc76335d7ae2ef074a9b30b02dff754e
李潇 <iPhone4s2008@iCloud.com>
2021-11-11 12:51:41 +0800

    2 正确，1错误，1 超时，1 运行时错误

---
+++ C++/DataStructure/307.cpp
@@ -0,0 +1,29 @@
+#pragma GCC diagnostic error "-std=c++14"
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+  stack<char> stk;
+  string str;
+  cin >> str;
+  bool flag = true;
+  for (char c : str) {
+    if (c == '(' || c == '[' || c == '{') {
+      stk.push(c);
+    }
+    if (c == ')' || c == ']' || c == '}') {
+      if (stk.top() != c - 1 && stk.top() != c - 2) {
+        cout << "wrong";
+        flag = false;
+        break;
+      }
+      stk.pop();
+    }
+  }
+  if (flag) {
+    cout << "right";
+  }
+}
