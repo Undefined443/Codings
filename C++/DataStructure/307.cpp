daeb11f92a19e1727455991e0c1de73f59a1e2d0
李潇 <iPhone4s2008@iCloud.com>
2021-11-14 00:27:49 +0800

    完成了 307

--- C++/DataStructure/307.cpp
+++ C++/DataStructure/307.cpp
@@ -6,24 +6,26 @@
 using namespace std;
 
 int main() {
-    stack<char> stk;
     string str;
-    cin >> str;
+    getline(cin, str);
+    stack<char> stk;
     bool flag = true;
     for (char c: str) {
         if (c == '(' || c == '[' || c == '{') {
             stk.push(c);
-        }
-        if (c == ')' || c == ']' || c == '}') {
-            if (stk.top() != c - 1 && stk.top() != c - 2) {
-                cout << "wrong";
+        } else if (c == ')' || c == ']' || c == '}') {
+            if (stk.empty() || (stk.top() != c - 1 && stk.top() != c - 2)) { //一定要注意 stack.empty() 的情况
+                cout << "wrong" << endl;
                 flag = false;
-                break;
+                return 0;
             }
             stk.pop();
         }
     }
-    if (flag) {
-        cout << "right";
+    if (flag && stk.empty()) {
+        cout << "right" << endl;
+    } else {
+        cout << "wrong" << endl;
     }
+
 }
