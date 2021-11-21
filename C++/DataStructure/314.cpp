6c87fdb9b86c64c2ed57283106b28ccdb3a35551
李潇 <iPhone4s2008@iCloud.com>
2021-11-21 10:23:13 +0800

    完成了 314

--- C++/DataStructure/314.cpp
+++ C++/DataStructure/314.cpp
@@ -5,21 +5,23 @@
 
 using namespace std;
 
-int calc(unsigned m, unsigned n) {
-    unsigned m_layer = log2(m); //计算 m 在二叉树的第几层。从 0 开始计数
-    unsigned n_layer = log2(n); //计算 n 在二叉树的第几层。
-    unsigned depth = n_layer - m_layer; //m 所在的子树的深度（这里平凡树的深度为 0）
-    unsigned bottom = exp2(depth - 1); 的上一层有多少元素
-    unsigned ans = exp2(depth) - 1; //在 m 子树中，n 所在层上面有多少元素
-    int front = m * exp2(depth);
+unsigned calc(unsigned m, unsigned n) {
+    auto m_layer = (unsigned) log2(m);           //计算 m 在二叉树的第几层。从 0 开始计数
+    auto n_layer = (unsigned) log2(n);           //计算 n 在二叉树的第几层
+    unsigned depth = n_layer - m_layer;          // m 子树的深度（平凡树的深度为 0）
+    unsigned ans = (unsigned) exp2(depth) - 1;   //在 m 子树中，n 所在层上面有多少元素
+    unsigned front = m * (unsigned) exp2(depth); //在 m 子树中，n 所在层的第一个元素的索引值
     ans += (n >= front) ? n - front + 1 : 0;
+    unsigned back = front + (front / m) - 1;     //在 m 子树中，n 所在层的最后一个元素的索引值。(front / m) 是 n 所在行的元素数
+    ans -= (n >= back) ? n - back : 0;
     return ans;
 }
 
 int main() {
     unsigned m, n;
-    do {
-        cin >> m >> n;
+    cin >> m >> n;
+    while (!((m == 0) && (n == 0))) {
         cout << calc(m, n) << endl;
-    } while (!((m == 0) && (n == 0)));
+        cin >> m >> n;
+    }
 }
