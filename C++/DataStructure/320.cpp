3071b3a18ce30e0ede37b360683b6904144053ce
李潇 <iPhone4s2008@iCloud.com>
2021-11-24 19:33:51 +0800

    temp

--- C++/DataStructure/320.cpp
+++ C++/DataStructure/320.cpp
@@ -1,3 +1,5 @@
+#pragma GCC diagnostic error "-std=c++11"
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -9,21 +11,22 @@ using iter = vector<int>::iterator;
 class Node {
 public:
     int m_val;
-    Node *lchild, *rchild;
-    
-    Node(int val);
+    Node *lchild{}, *rchild{};
+
+    explicit Node(int val);
 };
 
 Node::Node(int val) : m_val(val) {}
 
-//在分析时，将整个二叉树分解成只有根结点和左右孩子的最小子树
-Node *createBiTreeByLTAndLVR(iter LT_begin, iter LT_end, iter LVR_begin, iter LVR_end) { //返回由 [LVR_begin, LVR_end) 构建的二叉树的根结点；LT 是层序，LVR 是中序
+//返回由 [LVR_begin, LVR_end) 构建的二叉树的根结点；LT 是层序，LVR 是中序
+Node *createBiTreeByLTAndLVR(const vector<int> &LT, iter LVR_begin, iter LVR_end) { //在分析时，将整个二叉树分解成只有根结点和左右孩子的最小子树
+    static auto it = LT.begin();
     Node *root{};
     if (LVR_begin < LVR_end) {
-      root = new Node(*LT_begin); //层序的首结点为根结点
-      iter LVR_root = find(LVR_begin, LVR_end, root->m_val); //在中序中定位根结点
-      root->lchild = createTreeByLTAndLVR(LT_begin + 1, LT_end, LVR_begin, LVR_root);
-      root->rchild = createTreeByLTAndLVR(LT_begin + 2, LT_end, LVR_root + 1, LVR_end);
+        root = new Node(*LT_begin); //层序的首结点为根结点
+        auto LVR_root = find(LVR_begin, LVR_end, root->m_val); //在中序中定位根结点
+        root->lchild = createBiTreeByLTAndLVR(LT_begin + 1, LT_end, LVR_begin, LVR_root);
+        root->rchild = createBiTreeByLTAndLVR(LT_begin + 2, LT_end, LVR_root + 1, LVR_end);
     }
     return root;
 }
