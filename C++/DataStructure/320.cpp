ba7b72fe3519da616e99eb2320ad7d0badf99e12
李潇 <iPhone4s2008@iCloud.com>
2021-11-25 15:08:01 +0800

    完成了 320

--- C++/DataStructure/320.cpp
+++ C++/DataStructure/320.cpp
@@ -20,15 +20,30 @@ Node::Node(int val) : m_val(val) {}
 
 //返回由 [LVR_begin, LVR_end) 构建的二叉树的根结点；LT 是层序，LVR 是中序
 Node *createBiTreeByLTAndLVR(const vector<int> &LT, iter LVR_begin, iter LVR_end) { //在分析时，将整个二叉树分解成只有根结点和左右孩子的最小子树
-    static auto it = LT.begin();
     Node *root{};
     if (LVR_begin < LVR_end) {
-        root = new Node(*LT_begin); //层序的首结点为根结点
-        auto LVR_root = find(LVR_begin, LVR_end, root->m_val); //在中序中定位根结点
-        root->lchild = createBiTreeByLTAndLVR(LT_begin + 1, LT_end, LVR_begin, LVR_root);
-        root->rchild = createBiTreeByLTAndLVR(LT_begin + 2, LT_end, LVR_root + 1, LVR_end);
+        iter LVR_root;
+        for (int i: LT) {
+            if ((LVR_root = find(LVR_begin, LVR_end, i)) != LVR_end) { //遍历层序，第一个属于该子树的结点就是该子树的根结点
+                break;
+            }
+        }
+        root = new Node(*LVR_root);
+        root->lchild = createBiTreeByLTAndLVR(LT, LVR_begin, LVR_root);
+        root->rchild = createBiTreeByLTAndLVR(LT, LVR_root + 1, LVR_end);
     }
     return root;
+}
+
+void printLeaf(Node *node) {
+    if (node) {
+        if (!node->lchild && !node->rchild) {
+            cout << node->m_val << " ";
+        } else {
+            printLeaf(node->lchild);
+            printLeaf(node->rchild);
+        }
+    }
 }
 
 void PreOrderTraverse(Node *node) {
@@ -41,8 +56,8 @@ void PreOrderTraverse(Node *node) {
 
 void PostOrderTraverse(Node *node) {
     if (node) {
-        PreOrderTraverse(node->lchild);
-        PreOrderTraverse(node->rchild);
+        PostOrderTraverse(node->lchild);
+        PostOrderTraverse(node->rchild);
         cout << node->m_val << " ";
     }
 }
@@ -63,7 +78,9 @@ int main() {
         cin >> temp;
         LVR.push_back(temp);
     }
-    Node *root = createBiTreeByLTAndLVR(LT.begin(), LT.end(), LVR.begin(), LVR.end());
+    Node *root = createBiTreeByLTAndLVR(LT, LVR.begin(), LVR.end());
+    printLeaf(root);
+    cout << endl;
     PreOrderTraverse(root);
     cout << endl;
     PostOrderTraverse(root);
