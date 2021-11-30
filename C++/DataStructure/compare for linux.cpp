15a997f96b04983566a41bf90024a3b86bb69681
李潇 <iPhone4s2008@iCloud.com>
2021-11-30 18:46:07 +0800

    增加了 compare for linux

---
+++ C++/DataStructure/compare for linux.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ifstream file1(argv[1], ios::in);
+    ifstream file2(argv[2], ios::in);
+    string str1;
+    string str2;
+    int line = 1;
+    while (file1.good() && file2.good()) {
+        getline(file1, str1);
+        getline(file2, str2);
+        if (str1 != str2) {
+            cerr << "Difference at line " << line << ":" << endl;
+            cout << str1 << endl;
+            cout << str2 << endl
+        }
+        ++line;
+    }
+}
